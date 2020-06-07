643b4b689ec49507ff3d6ad06e93a1725102cbdf
skuma022 <suryaguru@live.com>
2020-06-07 02:19:02 -0700

    Finished Basic UI

--- partB/src/main/java/edu/ucr/cs/cs172/UI.java
+++ partB/src/main/java/edu/ucr/cs/cs172/UI.java
@@ -1,15 +1,21 @@
 package edu.ucr.cs.cs172;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 
 public class UI extends JFrame {
 	JPanel jp = new JPanel();
 	JLabel jl = new JLabel();
 	JTextField jt = new JTextField(30);
+	JButton jb = new JButton("Enter");
 
 	public UI() {
 	setTitle("Search");
@@ -18,6 +24,22 @@ public class UI extends JFrame {
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	jp.add(jt);
+
+	jt.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent temp) {
+			String userInput = jt.getText();
+			jl.setText(userInput);
+			}
+		});
+
+	jp.add(jb);
+	jb.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent temp) {
+			String input = jt.getText();
+			jl.setText(input);
+		}
+		});
+	jp.add(jl);
 	add(jp);
 	}
 
